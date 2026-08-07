5e2c9137c2a494c1fe4b77baa6f991d61b215082
otladA <adaltosrc@gmail.com>
2026-08-07 16:47:54 -0300

    move background init into get_bg_id helper

--- include/graphics.h
+++ include/graphics.h
@@ -18,18 +18,19 @@ typedef struct{
 } Tile;
 
 typedef struct{
-    u32 *data;
+    unsigned int *data;
     u32 length;
 } Bitmap;
 
 typedef struct{
     GfxMode gfx_mode;
     EngineMode engine_mode;
-    u32 *palette_data;
-    u32 palette_len;
+    short unsigned int *palette_data;
+    short unsigned int palette_len;
     BgType type;
     BgSize size;
     int id;
+    int layer;
     int palette_offset;
 
     union{
@@ -39,7 +40,10 @@ typedef struct{
 
 } Background;
 
-void bg_load(int screen, int bg_layer, Background *bg);
-// void bg_fade_out();
-// void bg_fade_in();
-// void bg_transition();
+void bg_load(Background *bg);
+int get_bg_id(Background *bg);
+
+// TODO:
+//      void bg_fade_out();
+//      void bg_fade_in();
+//      void bg_transition();
--- src/graphics.c
+++ src/graphics.c
@@ -1,20 +1,13 @@
 #include "graphics.h"
 
-void bg_load(int screen, int bg_layer, Background *bg){
+void bg_load(Background *bg){
     if (bg == NULL){
         return;
     }
 
-    // Get ID
-    if (bg->engine_mode != MAIN){
-        bg->id = bgInitSub(bg_layer, bg->type, bg->size, 0, 0);
-    } else{
-        bg->id = bgInit(bg_layer, bg->type, bg->size, 0, 0);
-    }
-
     switch (bg->gfx_mode){
         case GFX_MODE_BITMAP:
-            dmaCopy(bg->bitmap.data, bgGetGfxPtr(bg->id), bg->bitmap.length);
+            dmaCopy(bg->bitmap.data, bgGetGfxPtr(get_bg_id(bg)), bg->bitmap.length);
 
             if (bg->engine_mode != MAIN){
                 dmaCopy(bg->palette_data, BG_PALETTE_SUB + bg->palette_offset, bg->palette_len);
@@ -31,6 +24,23 @@ void bg_load(int screen, int bg_layer, Background *bg){
         default:
             break;
     }
+}
 
+int get_bg_id(Background *bg){
 
+    switch (bg->engine_mode){
+        case MAIN:
+            bg->id = bgInit(bg->layer, bg->type, bg->size, 0, 0);
+            return bg->id;
+            break;
+        
+        case SUB:
+            bg->id = bgInitSub(bg->layer, bg->type, bg->size, 0, 0);
+            return bg->id;
+            break;
+        
+        default:
+            return 0;
+            break;
+    }
 }
