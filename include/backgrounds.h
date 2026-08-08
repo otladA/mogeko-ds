2bf6f452ef533fe8defae4db72368a70881634cd
otladA <adaltosrc@gmail.com>
2026-08-07 21:29:01 -0300

    update backgrounds module to contain new assets for intro

--- include/backgrounds.h
+++ include/backgrounds.h
@@ -3,8 +3,13 @@
 #include "graphics.h"
 
 typedef enum{
+    BG_FUNAMUSEA_LOGO,
+    BG_MOGEKO_WARNING,
     BG_MOGEBED,
-    DIAL_YONAKA_IDLE
+    // Dialogue Sprites
+    // Yes, I know using them as backgrounds doesnt really make sense
+    // but it works
+    DIAL_YONAKA_IDLE,
 } BackgroundId;
 
 Background *get_background(BackgroundId id);
--- src/backgrounds.c
+++ src/backgrounds.c
@@ -2,8 +2,24 @@
 
 #include "mogebed.h"
 #include "yonaka_dialogue.h"
+#include "mogeko_warning.h"
+#include "funamusea_logo.h"
 
 Background registry[] = {
+    [BG_FUNAMUSEA_LOGO] = {
+        .gfx_mode = GFX_MODE_BITMAP, .engine_mode = MAIN, .layer = 2,
+        .type = BgType_Bmp8, .size = BgSize_B8_256x256,
+        .palette_data = funamusea_logoPal, .palette_len = funamusea_logoPalLen,
+        .bitmap = {funamusea_logoBitmap, funamusea_logoBitmapLen},
+    },
+
+    [BG_MOGEKO_WARNING] = {
+        .gfx_mode = GFX_MODE_BITMAP, .engine_mode = MAIN, .layer = 2,
+        .type = BgType_Bmp8, .size = BgSize_B8_256x256,
+        .palette_data = mogeko_warningPal, .palette_len = mogeko_warningPalLen,
+        .bitmap = {mogeko_warningBitmap, mogeko_warningBitmapLen},
+    },
+    
     [BG_MOGEBED] = {
         .gfx_mode = GFX_MODE_BITMAP, .engine_mode = MAIN, .layer = 2,
         .type = BgType_Bmp8, .size = BgSize_B8_256x256,
