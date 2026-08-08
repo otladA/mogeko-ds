c422fef5e2379d5880fbdbf59a4f8c7892719f42
otladA <adaltosrc@gmail.com>
2026-08-07 22:33:18 -0300

    add play_sfx helper with panning enum

--- include/audio.h
+++ include/audio.h
@@ -4,4 +4,12 @@
 #include "soundbank.h"
 #include "soundbank_bin.h"
 
+typedef enum{
+    LEFT = 0,
+    CENTER = 127,
+    RIGHT = 255
+} Panning;
+
 void play_song(mm_word song_id, bool loop);
+
+void play_sfx(mm_word sfx_id, int volume, Panning panning);
--- src/audio.c
+++ src/audio.c
@@ -7,3 +7,17 @@ void play_song(mm_word song_id, bool loop){
     mmLoad(song_id);
 	mmStart(song_id, loop ? MM_PLAY_LOOP : MM_PLAY_ONCE);
 }
+
+void play_sfx(mm_word sfx_id, int volume, Panning panning){
+    mmLoadEffect(sfx_id);
+
+    mm_sound_effect sfx = {
+        { sfx_id },
+        (int)(1.0f * (1<<10)),  // rate
+        0,                      // handle
+        volume,                 
+        panning,
+    };
+
+    mmEffectEx(&sfx);
+}
