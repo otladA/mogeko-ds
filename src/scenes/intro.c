1f5bcda4e35ed6418d0184bfdcdabc133b2cd231
otladA <adaltosrc@gmail.com>
2026-08-10 09:37:01 -0300

    use portrait registry and new graphics API in intro

--- src/scenes/intro.c
+++ src/scenes/intro.c
@@ -1,8 +1,10 @@
-#include "intro.h"
-#include "graphics.h"
-#include "audio.h"
-#include "backgrounds.h"
 #include <nds.h>
+
+#include <intro.h>
+#include <graphics.h>
+#include <audio.h>
+#include <backgrounds.h>
+#include <char_portraits.h>
 
 typedef enum{
     INTRO_FADE_IN,
@@ -20,8 +22,8 @@ IntroState *intro_init(void){
     intro_state.timer = 0;
     intro_state.finished = 0;
 
-    bg_load(get_background(BG_FUNAMUSEA_LOGO));
-    bg_fade_in(MAIN, 3000, 64);
+    img_load(get_background(BG_FUNAMUSEA_LOGO));
+    img_fade_in(MAIN, 3000, 64);
 
     return &intro_state;
 }
@@ -40,7 +42,7 @@ void intro_update(IntroState *state){
 
         case INTRO_LOGO:
             if (state->timer > 120){
-                bg_transition(MAIN, get_background(BG_MOGEKO_WARNING), 64);
+                img_transition(MAIN, get_background(BG_MOGEKO_WARNING), 64);
                 play_sfx(SFX_MOGEKO_WARNING, 255, CENTER);
                 state->state = INTRO_WARNING;
                 state->timer = 0;
@@ -51,8 +53,8 @@ void intro_update(IntroState *state){
         case INTRO_WARNING:
             if (state->timer > 650 || (keysDown() &KEY_A)){
                 audio_cleanup();
-                bg_transition(MAIN, get_background(BG_MOGEBED), 64);
-                bg_transition(SUB, get_background(DIAL_YONAKA_IDLE), 64);
+                img_transition(MAIN, get_background(BG_MOGEBED), 64);
+                img_transition(SUB, (get_char_portrait(YONAKA_IDLE)), 64);
                 play_song(MOD_MAIN_THEME, true);
                 state->state = INTRO_STORY;
                 state->timer = 0;
@@ -62,7 +64,7 @@ void intro_update(IntroState *state){
 
         case INTRO_STORY:
             if (state->timer > 600 || (keysDown() &KEY_A)){
-                bg_fade_out(BOTH, 3000, 64);
+                img_fade_out(BOTH, 3000, 64);
                 state->state = INTRO_FADE_OUT;
                 state->timer = 0;
             }
