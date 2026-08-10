617d7fcbf6d0f05d48ca58106a4ec409bcae3c93
otladA <adaltosrc@gmail.com>
2026-08-10 20:21:17 -0300

    add strange mogeko as a character

--- include/assets/characters.h
+++ include/assets/characters.h
@@ -3,13 +3,17 @@
 #include <graphics.h>
 
 typedef enum{
-    YONAKA,
-    
+    YONAKA = 0,
+    STRANGE_MOGEKO = 37,
 } CharacterId;
 
 typedef enum{
     // Yonaka
-    YONAKA_IDLE,
+    YONAKA_IDLE = 0,
+
+    // Strange Mogeko
+    STRANGE_MOGE_IDLE = 37,
+    STRANGE_MOGE_THOUGHTFUL,
 } CharacterPortraitId;
 
 Image *get_char_portrait(CharacterPortraitId id);
--- src/assets/characters.c
+++ src/assets/characters.c
@@ -1,13 +1,31 @@
 #include "characters.h"
 
 #include "yonaka_dialogue_idle.h"
+#include "strange_moge_idle.h"
+#include "strange_moge_thoughtful.h"
 
 Image char_registry[] = {
+    // Yonaka
     [YONAKA_IDLE] = {
         .gfx_mode = GFX_MODE_BITMAP, .engine_mode = SUB, .layer = 2,
         .type = BgType_Bmp8, .size = BgSize_B8_256x256,
         .palette_data = yonaka_dialogue_idlePal, .palette_len = yonaka_dialogue_idlePalLen, .palette_offset = 16,
         .bitmap = {yonaka_dialogue_idleBitmap, yonaka_dialogue_idleBitmapLen, 0}
+    },
+
+    // Strange Mogeko
+    [STRANGE_MOGE_IDLE] = {
+        .gfx_mode = GFX_MODE_BITMAP, .engine_mode = SUB, .layer = 2,
+        .type = BgType_Bmp8, .size = BgSize_B8_256x256,
+        .palette_data = strange_moge_idlePal, .palette_len = strange_moge_idlePalLen, .palette_offset = 0,
+        .bitmap = {strange_moge_idleBitmap, strange_moge_idleBitmapLen, 0}
+    },
+
+    [STRANGE_MOGE_THOUGHTFUL] = {
+        .gfx_mode = GFX_MODE_BITMAP, .engine_mode = SUB, .layer = 2,
+        .type = BgType_Bmp8, .size = BgSize_B8_256x256,
+        .palette_data = strange_moge_thoughtfulPal, .palette_len = strange_moge_thoughtfulPalLen, .palette_offset = 0,
+        .bitmap = {strange_moge_thoughtfulBitmap, strange_moge_thoughtfulBitmapLen, 0}
     },
 };
 
