27a033c5c3738963007c79cc9c01fa306d4fd7c2
otladA <adaltosrc@gmail.com>
2026-08-10 09:36:26 -0300

    remove dialogue portrait from backgrounds module

--- include/assets/backgrounds.h
+++ include/assets/backgrounds.h
@@ -6,10 +6,6 @@ typedef enum{
     BG_FUNAMUSEA_LOGO,
     BG_MOGEKO_WARNING,
     BG_MOGEBED,
-    // Dialogue Sprites
-    // Yes, I know using them as backgrounds doesnt really make sense
-    // but it works
-    DIAL_YONAKA_IDLE,
 } BackgroundId;
 
-Background *get_background(BackgroundId id);
+Image *get_background(BackgroundId id);
--- src/assets/backgrounds.c
+++ src/assets/backgrounds.c
@@ -5,7 +5,7 @@
 #include "mogeko_warning.h"
 #include "funamusea_logo.h"
 
-Background registry[] = {
+Image bg_registry[] = {
     [BG_FUNAMUSEA_LOGO] = {
         .gfx_mode = GFX_MODE_BITMAP, .engine_mode = MAIN, .layer = 2,
         .type = BgType_Bmp8, .size = BgSize_B8_256x256,
@@ -26,15 +26,8 @@ Background registry[] = {
         .palette_data = mogebedPal, .palette_len = mogebedPalLen,
         .bitmap = {mogebedBitmap, mogebedBitmapLen},
     },
-    
-    [DIAL_YONAKA_IDLE] = {
-        .gfx_mode = GFX_MODE_BITMAP, .engine_mode = SUB, .layer = 2,
-        .type = BgType_Bmp8, .size = BgSize_B8_256x256,
-        .palette_data = yonaka_dialoguePal, .palette_len = yonaka_dialoguePalLen, .palette_offset = 16,
-        .bitmap = {yonaka_dialogueBitmap, yonaka_dialogueBitmapLen},
-    }
 };
 
-Background *get_background(BackgroundId id){
-    return &registry[id];
+Image *get_background(BackgroundId id){
+    return &bg_registry[id];
 }
