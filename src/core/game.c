e0fd4bcfb09d509e680afb64ccdc9d724da5245b
otladA <adaltosrc@gmail.com>
2026-08-26 14:36:39 -0300

    add scene manager integration

--- src/core/game.c
+++ src/core/game.c
@@ -4,6 +4,7 @@
 #include <audio.h>
 #include <game.h>
 #include <intro.h>
+#include <title_screen.h>
 
 static volatile int frame = 0;
 
@@ -17,17 +18,30 @@ void game_init(GameContext *ctx){
 	videoSetMode(MODE_5_2D);
 	videoSetModeSub(MODE_5_2D);
 
-	vramSetBankA(VRAM_A_MAIN_BG);	// 128kb
-	vramSetBankC(VRAM_C_SUB_BG);	// 128kb
+	vramSetBankA(VRAM_A_MAIN_BG);		// 128kb
+	vramSetBankB(VRAM_B_MAIN_SPRITE);	// 128kb
+	vramSetBankC(VRAM_C_SUB_BG);		// 128kb
 
-	ctx->current_scene = SCENE_INTRO;
-	ctx->scene_state = intro_init();
+	// SpriteMapping_1D_128 : consecutive tiles are in blocks of 128 bytes in VRAM
+	oamInit(&oamMain, SpriteMapping_1D_128, false);
+
+	ctx->current_scene = SCENE_TITLE_SCR;
+	ctx->scene_state = title_init();
 }
 
 void game_update(GameContext *ctx){
-    scanKeys();
-
 	switch(ctx->current_scene){
+		case SCENE_TITLE_SCR:
+			TitleState *title = (TitleState*)ctx->scene_state;
+
+			title_update(title);
+
+			if (title_is_finished(title)){
+				scene_change(ctx, SCENE_INTRO);
+			}
+
+			break;
+		
 		case SCENE_INTRO:
 			IntroState *intro = (IntroState*)ctx->scene_state;
 			intro_update(intro);
--- src/core/scene_manager.c
+++ src/core/scene_manager.c
@@ -1,10 +1,15 @@
 #include <scene_manager.h>
 #include <game.h>
 
+#include <title_screen.h>
 #include <intro.h>
 
 void scene_change(GameContext *ctx, SceneID new_scene){
 	switch (ctx->current_scene){
+		case SCENE_TITLE_SCR:
+			title_cleanup((TitleState*)ctx->scene_state);
+			break;
+
 		case SCENE_INTRO:
 			intro_cleanup((IntroState*)ctx->scene_state);
 			break;
@@ -15,6 +20,10 @@ void scene_change(GameContext *ctx, SceneID new_scene){
 
 	ctx->current_scene = new_scene;
 	switch (new_scene){
+		case SCENE_TITLE_SCR:
+			ctx->scene_state = title_init();
+			break;
+
 		case SCENE_INTRO:
 			ctx->scene_state = intro_init();
 			break;
