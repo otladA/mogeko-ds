54904f12d2697668285a3f8be0a3fd9a4656b67d
otladA <adaltosrc@gmail.com>
2026-09-02 17:56:23 -0300

    remove unused yonaka_spr entity

--- src/entities/yonaka_spr.c
+++
@@ -1,6 +0,0 @@
-#include <yonaka_spr.h>
-
-void init_yonaka_spr(Yonaka *sprite, u8* gfx){
-	sprite->sprite_gfx_mem = oamAllocateGfx(&oamMain, SpriteSize_64x64, SpriteColorFormat_256Color);
-	sprite->frame_gfx = (u8*)gfx;
-}
